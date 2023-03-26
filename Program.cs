58097a74ef1c3f04d3b1beb9fbb3294f7ab67349
unknown <aselturar2000@gmail.com>
2023-03-26 16:53:12 +0600

    решение дз

--- Program.cs
+++ Program.cs
@@ -1,33 +1,91 @@
-﻿// Задача 1: напишите программу, которая на вход принимает число и выдает его квадрат 
-//int n = Convert.ToInt32(Console.ReadLine() );
-//Console.WriteLine(n*n);
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 
-//Задача 2: напишите программу, которая на вход принимает два числа и проверяет является ли первое число квадратои второго
-//int n1 = Convert.ToInt32(Console.ReadLine());
-//int n2 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
 
-//if (n1==n2*n2)
- // Console.WriteLine("yes");
-//else
- // Console.WriteLine("no");
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
 
- //Задача 3: напишите программу, которая будет выдавать название дня недели по заданному номеру
-int DayNumber = Convert.ToInt32(Console.ReadLine());
-if (DayNumber == 1)
-  Console.WriteLine("понедельник");
-else if (DayNumber == 2)
-     Console.WriteLine("вторник");
-else if (DayNumber == 3)
-     Console.WriteLine("среда");
-else if (DayNumber == 4)
-     Console.WriteLine("четверг");
-else if (DayNumber == 5)
-     Console.WriteLine("пятница");
-else if (DayNumber == 6)
-     Console.WriteLine("суббота");
-else if (DayNumber == 7)
-     Console.WriteLine("воскресенье");
+if(firstNumber > secondNumber)
+{
+    max = firstNumber;
+    min = secondNumber;
+}
 else
-  Console.WriteLine("в неделе не больше семи дней");
+{
+    max = secondNumber;
+    min = firstNumber;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
+
+
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. (Сравнить с каждым)
+
+int max = 0;
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > max)
+{
+    max = firstNumber;
+}
+if(secondNumber > max)
+{
+    max = secondNumber;
+}
+if(thirdNumber > max)
+{
+    max = thirdNumber;
+}
+
+Console.WriteLine("max = " + max);
+
+
+
+
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
+
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
